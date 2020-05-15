79b17d6953e205bd0bd9fd5108055901529f5759
RubenMarVil <ruben.marquez@alu.uclm.es>
2020-05-15 22:04:49 +0200

    [src] Añadido diseño inicial de la interfaz para la obtencion del nivel del usuario

---
+++ GLOBAL-MANAGER/Assets/src/SliderHandleAge.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class SliderHandleAge : MonoBehaviour
+{
+    public Text dynamicTxtAge;
+
+    private void Start()
+    {
+        dynamicTxtAge = GetComponent<Text>();
+    }
+
+    public void OnSliderValueChanged(float ageNumber)
+    {
+        dynamicTxtAge.text = ageNumber.ToString();
+    }
+}
