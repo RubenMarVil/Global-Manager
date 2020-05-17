005dde6c28beb38326b028f96c7b5ed8ce596567
RubenMarVil <ruben.marquez@alu.uclm.es>
2020-05-17 18:38:09 +0200

    [src] Añadida pregunta en el cuestionario de nivel con respuestas de ordenacion

---
+++ GLOBAL-MANAGER/Assets/src/ChallangeHandle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OrderChallange;
+using System;
+using UnityEngine.Experimental.UIElements;
+
+public class ChallangeHandle : MonoBehaviour
+{
+    public void ChallangeClick()
+    {
+        OrderChallange.ChallengesOrderHandle.SetChallangeClick(Int32.Parse(name));
+    }
+}
---
+++ GLOBAL-MANAGER/Assets/src/ChallengesOrderHandle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OrderChallange
+{
+    public class ChallengesOrderHandle : MonoBehaviour
+    {
+        private static Text[] challangeNames;
+
+        private static int challangeToChange;
+        private static int challangeFromChange;
+        private static bool changing;
+
+        void Start()
+        {
+            challangeNames = GetComponentsInChildren<Text>();
+            changing = false;
+        }
+
+        public static void SetChallangeClick(int challange)
+        {
+            if (!changing)
+            {
+                challangeToChange = challange - 1;
+                changing = true;
+                challangeNames[challangeToChange].GetComponentInParent<Image>().color = Color.grey;
+            }
+            else
+            {
+                challangeFromChange = challange - 1;
+                ChangeChallenges();
+                changing = false;
+                challangeNames[challangeToChange].GetComponentInParent<Image>().color = Color.white;
+            }
+        }
+
+        private static void ChangeChallenges()
+        {
+            string aux = challangeNames[challangeFromChange].text;
+            challangeNames[challangeFromChange].text = challangeNames[challangeToChange].text;
+            challangeNames[challangeToChange].text = aux;
+        }
+    }
+}
