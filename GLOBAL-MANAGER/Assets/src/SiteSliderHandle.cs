1905f795d39cd6deee1a27c653a1cef984ee936f
RubenMarVil <ruben.marquez@alu.uclm.es>
2020-05-22 14:02:44 +0200

    [src] Añadido diseño primera configuracion de la fase de configuracion

---
+++ GLOBAL-MANAGER/Assets/src/SiteSliderHandle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SiteSliderHandle : MonoBehaviour
+{
+    public GameObject[] siteList;
+    private Vector2[] contentDimensions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contentDimensions = new[] {new Vector2(0, 409), new Vector2(0, 541), 
+            new Vector2(0, 665), new Vector2(0, 797), new Vector2(0, 927), new Vector2(0, 1054)};
+
+
+        
+    }
+
+    public void OnSliderValueChanged(float numSites)
+    {
+        Debug.Log($"Sites number: {numSites}");
+
+        for(int i = 0; i < siteList.Length; i++)
+        {
+            if(i < numSites)
+            {
+                siteList[i].SetActive(true);
+            }
+            else
+            {
+                siteList[i].SetActive(false);
+            }
+        }
+
+        GetComponent<RectTransform>().sizeDelta = contentDimensions[(int)numSites - 2];
+    }
+}
