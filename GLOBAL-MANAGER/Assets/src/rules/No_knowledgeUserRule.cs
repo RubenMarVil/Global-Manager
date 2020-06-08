48c86ec562af7fe73bf9b4395d90970a8fb34e7b
RubenMarVil <ruben.marquez@alu.uclm.es>
2020-06-08 18:09:21 +0200

    [src] Añadida integracion con el sistema de reglas NRules e implementadas algunas reglas para la obtención de nivel de usuario

---
+++ GLOBAL-MANAGER/Assets/src/rules/No_knowledgeUserRule.cs
@@ -0,0 +1,27 @@
+﻿using Assets.src.domain;
+using NRules.Fluent.Dsl;
+
+namespace Assets.src.rules
+{
+    [Name("No_KnowledgeUsersRule")]
+    [Tag("UserLevel")]
+    public class No_KnowledgeUserRule : Rule
+    {
+        public override void Define()
+        {
+            User user = null;
+
+            When()
+                .Match<User>(() => user, u => u.CulturalKnowledge == KnowledgeLevels.LOW,
+                u => u.LanguageKnowledge == KnowledgeLevels.LOW, u => u.timeKnowledge == KnowledgeLevels.LOW);
+
+            Then()
+                .Do(ctx => AddNo_KnowledgeLevel(user));
+        }
+
+        private static void AddNo_KnowledgeLevel(User user)
+        {
+            user.UserLevel = UserLevels.NO_KNOWLEDGE;
+        }
+    }
+}
